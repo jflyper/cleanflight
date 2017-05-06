05debf753de2df1ec8f788becdb8087da6332c0b
jflyper <shinoda@jaist.ac.jp>
2017-05-06 18:00:30 +0900

    Changed PWM5&6 to TIM1

--- src/main/target/OMNIWHOOPF4/target.c
+++ src/main/target/OMNIWHOOPF4/target.c
@@ -30,7 +30,7 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM3, CH1, PA6,  TIM_USE_MOTOR,               1, 0),  // PWM2
     DEF_TIM(TIM4, CH3, PB8,  TIM_USE_MOTOR,               1, 0),  // PWM3
     DEF_TIM(TIM3, CH4, PB1,  TIM_USE_MOTOR,               1, 0),  // PWM4
-    DEF_TIM(TIM3, CH2, PA7,  TIM_USE_MOTOR,               1, 0),  // PWM5
-    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_MOTOR,               1, 0),  // PWM6
+    DEF_TIM(TIM1, CH1N,PA7,  TIM_USE_MOTOR,               TIMER_OUTPUT_INVERTED, 0),  // PWM5
+    DEF_TIM(TIM1, CH2N,PB0,  TIM_USE_MOTOR,               TIMER_OUTPUT_INVERTED, 0),  // PWM6
     DEF_TIM(TIM2, CH1, PA5,  TIM_USE_LED,                 1, 0),  // LED_STRIP
 };
